51c57affa7a682f489e59df58e45f34aee63708a
유경 <eeheueklf@gmail.com>
2026-01-28 23:03:54 +0900

    [build] 빌드 오류
    - 파일에 내용이 없을 경우 오류
    - 최소한만 남겨둠

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,36 +1,40 @@
-// "use client";
+"use client";
 
-// import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-// export default function Home() {
-//   const { data: session } = useSession();
+export default function Home() {
+  const { data: session } = useSession();
 
-//   return (
-//     <main style={{ padding: "50px", textAlign: "center" }}>
-//       {session ? (
-//         <div>
-//           <p>반갑습니다, <strong>{session.user?.name}</strong>님!</p>
-//           {session.user?.image && (
-//             <img 
-//               src={session.user.image} 
-//               alt="프로필" 
-//               style={{ borderRadius: "50%", width: "80px", marginBottom: "10px" }} 
-//             />
-//           )}
-//           <br />
-//           <button onClick={() => signOut()} style={{ padding: "10px 20px", cursor: "pointer" }}>
-//             로그아웃
-//           </button>
-//         </div>
-//       ) : (
-//         <div>
-//           <p>로그인이 필요합니다.</p>
-//           <button onClick={() => signIn("google")} style={{ padding: "10px 20px", cursor: "pointer" }}>
-//             구글로 시작하기
-//           </button>
-//         </div>
-//       )}
-//     </main>
-//   );
-// }
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      
+    </div>
+    // <main style={{ padding: "50px", textAlign: "center" }}>
+
+    //   {session ? (
+    //     <div>
+    //       <p>반갑습니다, <strong>{session.user?.name}</strong>님!</p>
+    //       {session.user?.image && (
+    //         <img 
+    //           src={session.user.image} 
+    //           alt="프로필" 
+    //           style={{ borderRadius: "50%", width: "80px", marginBottom: "10px" }} 
+    //         />
+    //       )}
+    //       <br />
+    //       <button onClick={() => signOut()} style={{ padding: "10px 20px", cursor: "pointer" }}>
+    //         로그아웃
+    //       </button>
+    //     </div>
+    //   ) : (
+    //     <div>
+    //       <p>로그인이 필요합니다.</p>
+    //       <button onClick={() => signIn("google")} style={{ padding: "10px 20px", cursor: "pointer" }}>
+    //         구글로 시작하기
+    //       </button>
+    //     </div>
+    //   )}
+    // </main>
+  );
+}
 
